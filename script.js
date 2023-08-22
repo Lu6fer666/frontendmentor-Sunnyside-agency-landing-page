07977ac9a348034fae33de17d1e596751dab4a06
Julien Milo <julienmilo369@gmail.com>
2023-08-22 17:04:20 +0200

    -3 days work

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const burgerMenu = document.querySelector(".burger");
+const burgerMenu = document.querySelector(".fa-solid.fa-bars");
+const hiddenMenu = document.querySelector(".header__nav__navbar");
 
+burgerMenu.addEventListener("click", function () {
+  hiddenMenu.classList.toggle("shown");
+  });
 
